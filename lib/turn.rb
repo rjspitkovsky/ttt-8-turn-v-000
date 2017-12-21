ba82a6c1ca10ecc61a460329aa48a9026f6cef01
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-21 20:44:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,3 +11,17 @@ def display_board(board)
    puts line3
 
  end
+
+def valid_move?(board, index) 
+    if index > -1 && index < 9 && position_taken?(board, index) == false
+      true 
+    end 
+end 
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true 
+  elsif board[index] == " " || "" || nil 
+    false 
+  end 
+end 
